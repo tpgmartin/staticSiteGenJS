4d93a46a04925d322c2a4985c20dae7eb3a41010
Tom Martin <tom@tpgmartin.com>
2015-03-24 15:13:41 +0000

    Create markdown file from terminal

--- tasks/new.js
+++ tasks/new.js
@@ -1,1 +1,25 @@
-var gulp = require('gulp');
+var gulp   = require('gulp'),
+    prompt = require('gulp-prompt'),
+    rename = require('gulp-rename'),
+    file   = require('gulp-file'),
+    shell  = require('gulp-shell'),
+    fs = require('fs');
+
+// Tasks:
+// * Prompt user to enter title
+// * Automatically generate datetime
+// * Create file name by concatenating title and datetime
+// * Output to _posts/
+
+gulp.task('new', function () {
+  return gulp.src('./_posts').pipe(prompt.prompt({
+        type: 'input',
+        name: 'title',
+        message: 'Post title'
+    }, function(path){
+      var filename = './_posts/git' + path.title + '.md'
+      fs.writeFile(filename , 'Some markup', function (err) {
+        if (err) throw err;
+      });
+    }))
+});
